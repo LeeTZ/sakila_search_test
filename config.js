f5eb086b2f0b5b4e3eb06e8223e95c3870ef8557
LeeTZ <litiezheng513@gmail.com>
2016-05-15 08:51:48 -0400

    add disconnect handler

--- config.js
+++ config.js
@@ -11,12 +11,38 @@ connection.connect();
 
 // database connection setup, should be made global
 var mysql = require('mysql');
-connection      =         mysql.createConnection({
+/*connection      =         mysql.createConnection({
         host        :         "us-cdbr-iron-east-04.cleardb.net",
         user        :         "bc929804294e20",
         password    :         "ef99ee42",
         database     :        "heroku_468468891c6d425"
-});
-connection.connect();
+});*/
+//connection.connect();
 
 
+function handleDisconnect() {
+    connection      =         mysql.createConnection({
+            host        :         "us-cdbr-iron-east-04.cleardb.net",
+            user        :         "bc929804294e20",
+            password    :         "ef99ee42",
+            database     :        "heroku_468468891c6d425"
+    });
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
