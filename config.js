4b5190a54bb560405c9b3a0b894934e9ee6f706c
LeeTZ <litiezheng513@gmail.com>
2016-05-15 08:54:35 -0400

    add disconnect handler

--- config.js
+++ config.js
@@ -1,31 +1,21 @@
 // database connection setup, should be made global
-/*var mysql = require('mysql');
-connection      =         mysql.createConnection({
-        host        :         "localhost",
-        user        :         "root",
-        password    :         "",
-        database     :         "sakila"
-});
-connection.connect();
-*/
-
-// database connection setup, should be made global
 var mysql = require('mysql');
-/*connection      =         mysql.createConnection({
-        host        :         "us-cdbr-iron-east-04.cleardb.net",
-        user        :         "bc929804294e20",
-        password    :         "ef99ee42",
-        database     :        "heroku_468468891c6d425"
-});*/
-//connection.connect();
-
 
 function handleDisconnect() {
+    // server
+    /*
     connection      =         mysql.createConnection({
             host        :         "us-cdbr-iron-east-04.cleardb.net",
             user        :         "bc929804294e20",
             password    :         "ef99ee42",
             database     :        "heroku_468468891c6d425"
+    });*/
+    // local
+    connection      =         mysql.createConnection({
+        host        :         "localhost",
+        user        :         "root",
+        password    :         "",
+        database     :         "sakila"
     });
 
   connection.connect(function(err) {              // The server is either down
