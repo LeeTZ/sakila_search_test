a9013ae5ac65407ddf10ef6c298cd3bef5527a4b
LeeTZ <litiezheng513@gmail.com>
2016-05-15 08:23:39 -0400

    modify config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // database connection setup, should be made global
-var mysql = require('mysql');
+/*var mysql = require('mysql');
 connection      =         mysql.createConnection({
         host        :         "localhost",
         user        :         "root",
@@ -7,3 +7,16 @@ connection      =         mysql.createConnection({
         database     :         "sakila"
 });
 connection.connect();
+*/
+
+// database connection setup, should be made global
+var mysql = require('mysql');
+connection      =         mysql.createConnection({
+        host        :         "us-cdbr-iron-east-04.cleardb.net",
+        user        :         "bc929804294e20",
+        password    :         "ef99ee42",
+        database     :        "heroku_468468891c6d425"
+});
+connection.connect();
+
+
